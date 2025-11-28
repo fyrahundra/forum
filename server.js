d9b0efb6ce9ed6ef34f8a8a36ad94d9f05d25033
fyrahundra <karlgrenjonathan@gmail.com>
2025-11-28 11:48:56 +0100

    ws rework 17

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 // server.js
 import { createServer } from 'http';
 import { handler } from './build/handler.js';
-import { attachWebSocketServer } from './build/server/chunks/hooks.server-BaIpZ9G8.js';
+import { attachWebSocketServer } from './.svelte-kit/output/server/chunks/hooks.server.js'; // <- JS output
 
 const PORT = process.env.PORT || 3000;
 
-// create HTTP server
 const server = createServer(handler);
 
-// attach WebSocket server
+// attach the WS server
 attachWebSocketServer(server);
 
 server.listen(PORT, '0.0.0.0', () => {
-	console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
