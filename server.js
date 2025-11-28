2825cb1acec108e9a19d91821ee82d7c3a7d9d5a
fyrahundra <karlgrenjonathan@gmail.com>
2025-11-28 09:55:20 +0100

    ws rework 4

--- server.js
+++ server.js
@@ -38,6 +38,7 @@ setInterval(() => {
     });
 }, 25000); // must be < Render timeout
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log(`Server listening on ${PORT}`);
 });
+
